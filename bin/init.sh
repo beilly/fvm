7178d407ec1969c45623a019c069fb2e14658bf8
beilly <shibenli@mmfintec.com>
2020-03-04 21:55:44 +0800

    brew self

--- bin/init.sh
+++ bin/init.sh
@@ -8,7 +8,7 @@ HOMEBREW_PROJECT="${THIS_DIR}/../packages/homebrew-fvm"
 
 function main(){
   if [[ ! -d $HOMEBREW_PROJECT ]];then
-    git clone https://github.com/dashixiong91/homebrew-fvm.git $HOMEBREW_PROJECT
+    git clone https://github.com/beilly/homebrew-fvm.git $HOMEBREW_PROJECT
   fi
   pushd $HOMEBREW_PROJECT
     git pull
